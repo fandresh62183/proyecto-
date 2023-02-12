caa1d7cec1a183d90065e9448d66f629c9bb006f
fandresh62183 <fandresh62183@gmail.com>
2023-02-11 19:52:55 -0500

    final

---
+++ src/Login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <form method="post" action="" name="signin-form">
+    <div class="form-element">
+        <label>Username</label>
+        <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+    </div>
+    <div class="form-element">
+        <label>Password</label>
+        <input type="password" name="password" required />
+    </div>
+    <button type="submit" name="login" value="login">Login</button>
+</form>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Login } from './Login/Login';
+import { Register } from './Register/Register';
 
-import {Vista} from './Estructura/Vista.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Vista></Vista>
+   <Login/>
+   <Register/>
   </React.StrictMode>
 );
 
